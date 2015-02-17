08f1de121f151d2fb364815b9ff29b81d2590979
Aman Madaan <madaan.amanmadaan@gmail.com>
2015-02-18 01:25:48 +0530

    add more comments to pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,9 +1,37 @@
 #the multir preprocessing pipeline
-FLATFILE=$1 #the initial flatfile
+
 ####################################################
 #Define the number of mappers/reducers for different
 #steps of the pipeline
+MAPPER_STD=10
+RED_STD=40
+PP_MAPPER=$MAPPER_STD
+PP_REDUCER=$RED_STD
 
+PREPARSE_MAPPER_COUNT=$MAPPER_STD
+PREPARSE_REDUCER_COUNT=$RED_STD
+
+DEPPARSE_MAPPER_COUNT=$MAPPER_STD
+CHUNKERMAPPER_COUNT=$MAPPER_STD
+MARKER_MAPPER_COUNT=$MAPPER_STD
+####################################################
+
+####################################################
+#Define the files to be used
+FLATFILE=$1 #the initial flatfile
+DOCIDFILE=$FLATFILE"IDS" #flat file with doc ids
+PPOUTPUT=$FLATFILE"_PPED"   #preprocessing input
+PREPARSEINPUT="$FLATFILE"_PPIN  #preparse processing input
+PREPARSEOUTPUT="$FLATFILE"_PPOUT    #preparse processing output
+PARSEINPUT=$FLATFILE"PARSEIN"   #parser input
+PARSEOUT=$FLATFILE"PARSEOUT"    #parser output
+DEPPARSEOUT=$FLATFILE"DEP"  #dependency parser output
+CHUNKERINFILE="$FLATFILE"CHUNKIN    #chunker input
+CHUNKEROUTFILE="$FLATFILE"CHUNKOUT  #chunker output
+MARKER_INFILE=$PARSEINPUT   #marker input
+MARKER_OUTFILE=$FLATFILE"MARKEROUT" #marker output
+COMB_INCORRECT_ORDER="$FLATFILE"MERGE   #merged file 
+COMB_CORRECT_ORDER="$FLATFILE"RES   #merged file with columns in correct order
 ####################################################
 
 echo "Processing $FLATFILE"
@@ -13,26 +41,21 @@ echo "Step1: Assign doc ids"
 echo "######################"
 
 LINES_PER_DOC=100
-DOCIDFILE=$FLATFILE"IDS"
-python assignDocIds.py "$FLATFILE" "$LINES_PER_DOC" > "$DOCIDFILE"
+python assignDocIds.py "$FLATFILE" "$LINES_PER_DOC" > "$DOCIDFILE" #add document id to each line
 echo -e "Assigning document ids to sentences, $LINES_PER_DOC sentences per doc\n\n"
 
 echo "######################"
 echo "Step2: feed to the multir preprocessor"
 echo -e "######################\n\n"
-PP_MAPPER=1
-PP_REDUCER=1
 echo "Starting input preprocessing"
 hadoop fs -copyFromLocal $DOCIDFILE $DOCIDFILE
 rm $DOCIDFILE
-PPOUTPUT=$FLATFILE"_PPED"
-bash runMultirPreprocess.sh $DOCIDFILE $PPOUTPUT $PP_MAPPER $PP_REDUCER
+bash runMultirPreprocess.sh $DOCIDFILE $PPOUTPUT $PP_MAPPER $PP_REDUCER #add metadata required by stages ahead
 echo "input preprocessing finished, copying files to disk, sorting and assigning sentence ids"
 hadoop fs -getmerge $PPOUTPUT $PPOUTPUT
 hadoop fs -rmr $PPOUTPUT
 sed -i '/^\s*$/d' $PPOUTPUT
-PREPARSEINPUT="$FLATFILE"_PPIN
-python assignsentids.py $PPOUTPUT > $PREPARSEINPUT
+python assignsentids.py $PPOUTPUT > $PREPARSEINPUT  #assign a sentence id to each sentence
 echo "Preparse input file created"
 
 rm $PPOUTPUT $FLATFILEIDS
@@ -44,9 +67,6 @@ echo -e "######################\n\n"
 echo -e "Starting preparse processing, file: $PREPARSEINPUT"
 echo "Copying file to HDFS"
 hadoop fs -copyFromLocal $PREPARSEINPUT $PREPARSEINPUT
-PREPARSEOUTPUT="$FLATFILE"_PPOUT
-PREPARSE_MAPPER_COUNT=1
-PREPARSE_REDUCER_COUNT=1
 echo "Spawing the job"
 bash runpre.sh  $PREPARSEINPUT $PREPARSEOUTPUT $PREPARSE_MAPPER_COUNT $PREPARSE_REDUCER_COUNT
 echo "Copying results to disk, cleaning HDFS"
@@ -59,8 +79,6 @@ echo "Step 4: Run Parsing"
 echo -e "######################\n\n"
 
 echo "starting parsing"
-PARSEINPUT=$FLATFILE"PARSEIN"
-PARSEOUT=$FLATFILE"PARSEOUT"
 cut -f 1,4 $PREPARSEOUTPUT > $PARSEINPUT
 hadoop fs -copyFromLocal $PARSEINPUT $PARSEINPUT
 bash hadoopmultir/parsing/run.sh $PARSEINPUT $PARSEOUT
@@ -71,8 +89,6 @@ hadoop fs -getmerge $PARSEOUT $PARSEOUT
 echo -e "\n\n######################"
 echo "Step 5: Run Post Parse Processing (Dependency parsing: possibly slow, some tasks will fail, that's okay)"
 echo -e "######################\n\n"
-DEPPARSEOUT=$FLATFILE"DEP"
-DEPPARSE_MAPPER_COUNT=1
 bash runPost.sh $PARSEOUT $DEPPARSEOUT $DEPPARSE_MAPPER_COUNT
 echo "finished"
 hadoop fs -getmerge $DEPPARSEOUT $DEPPARSEOUT
@@ -83,9 +99,6 @@ hadoop fs -rmr $PARSEOUT
 echo -e "\n\n######################"
 echo "Step 6: Run chunker"
 echo -e "######################\n\n"
-CHUNKERINFILE="$FLATFILE"CHUNKIN
-CHUNKEROUTFILE="$FLATFILE"CHUNKOUT
-CHUNKERMAPPER_COUNT=1
 
 echo "Preparing input file to be fed to chunker"
 join $PREPARSEINPUT $PREPARSEOUTPUT -t $'\t'|cut -f1,3,4,7 > $CHUNKERINFILE
@@ -100,9 +113,6 @@ hadoop fs -getmerge $CHUNKEROUTFILE $CHUNKEROUTFILE
 echo -e "\n\n######################"
 echo "Step 7: Run (country) Marker (make sure you apply your correct marker for this step)"
 echo -e "######################\n\n"
-MARKER_INFILE=$PARSEINPUT
-MARKER_OUTFILE=$FLATFILE"MARKEROUT"
-MARKER_MAPPER_COUNT=1
 echo "Spawning Marker"
 bash runMarker.sh $MARKER_INFILE $MARKER_OUTFILE $MARKER_MAPPER_COUNT
 hadoop fs -getmerge $MARKER_OUTFILE $MARKER_OUTFILE
@@ -111,8 +121,6 @@ echo "Marking over, getting data back to the disk"
 echo -e "\n\n######################"
 echo "Step 8: Combine all the results"
 echo -e "######################\n\n"
-COMB_INCORRECT_ORDER="$FLATFILE"MERGE
-COMB_CORRECT_ORDER="$FLATFILE"RES
 echo "Sorting preparseoutput"
 sort -nk1 $PREPARSEOUTPUT -o $PREPARSEOUTPUT
 echo "Sorting chunkeroutput"
@@ -129,10 +137,11 @@ awk -F $'\t' 'BEGIN {OFS = FS} {print $1,$2,$8,$5,$4,$9,$11,$12,$7,$3,$6,$10}' $
 rm $COMB_INCORRECT_ORDER
 echo "Finished!"
 echo "Removing files"
-rm $PREPARSEOUTPUT
-rm $PREPARSEINPUT
-rm $CHUNKEROUTFILE
-rm $MARKER_OUTFILE
-rm $DEPPARSEOUT
-
+hadoop fs -rmr $PREPARSEOUTPUT
+hadoop fs -rm $PREPARSEINPUT
+hadoop fs -rmr $CHUNKEROUTFILE
+hadoop fs -rmr $MARKER_OUTFILE
+hadoop fs -rmr $DEPPARSEOUT
+mv $COMB_CORRECT_ORDER RES"$FLATFILE"
+rm "$FLATFILE"[A-Z_]*
 echo "Result written to: $COMB_CORRECT_ORDER"
